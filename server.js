b1ba3236f4690afffdb1ccd438c3c5f575084362
Hylke Bons <hylkebons@gmail.com>
2011-06-14 01:05:57 +0100

    add license header

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+//
+//   SparkleShare-Web - An HTTP API to your SparkleShare server.
+//   Copyright (C) 2011 The SparkleShare Community  <http://www.sparkleshare.org>
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Affero General Public License as
+//   published by the Free Software Foundation, either version 3 of the
+//   License, or (at your option) any later version.
+//
+   
 var config = require('./config');
 var http = require('http');
 

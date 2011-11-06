be48ab4dc7ef19509cc22881c84e770d60f9d3bc
Martin -nexus- Filip <nexus@smoula.net>
2011-11-06 17:34:50 +0100

    corrected introduce message

--- app.js
+++ app.js
@@ -485,4 +485,4 @@ app.get('/getLinkCode', [middleware.isLogged, middleware.isAdmin], function(req,
 });
 
 app.listen(config.listen.port, config.listen.host);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("SparkleShare Dashboard listening on port %d in %s mode", app.address().port, app.settings.env);
